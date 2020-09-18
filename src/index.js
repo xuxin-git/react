f08b18516bd98b1b6fe84a407e6a017bdef8796d
xuxin <xu_xin96@163.com>
2020-09-18 16:00:10 +0800

    暴露配置文件 and 按需引入antd

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import {store} from './example/todoList/index'
 import App from './example/todoList/components/App'
+import {Button} from "antd";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+      <Button type={"primary"}>add</Button>
   </Provider>,
 
   document.getElementById('root')
