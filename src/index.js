b39582761badca0bd764c8490ac51922d9749ddb
xuxin <xu_xin96@163.com>
2020-09-18 17:10:34 +0800

    加入layout布局框架

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { Provider } from 'react-redux'
 import {store} from './example/todoList/index'
 import App from './example/todoList/components/App'
 import {Button} from "antd";
-
+import Home from "./home/home";
 ReactDOM.render(
   <Provider store={store}>
-      <Button type={"primary"}>add</Button>
+      <Home/>
   </Provider>,
 
   document.getElementById('root')
