efd6f23683832677b1e76a7b010ce38ee87d81aa
xuxin <xu_xin96@163.com>
2020-09-25 17:16:32 +0800

    新建test分支，新增目录

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import {store} from './example/todoList/index'
 import App from './example/todoList/components/App'
 import {Button} from "antd";
-import Home from "./home/home";
+import Home from "./demo/home";
 ReactDOM.render(
   <Provider store={store}>
       <Home/>
